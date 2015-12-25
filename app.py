fa9c9d317c11a2185543b4cb6ca38038aad6b0c4
brianleung <bleungwh@gmail.com>
2015-12-24 16:53:24 -0800

    install/setup database dependencies and rename files.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from user import User
 
 app = Flask(__name__)
-
-# main cached data
-users = {}
-groups = {}
-
 
 def is_valid(data):
     return ("first_name" in data 
